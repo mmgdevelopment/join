63f19a3a483bda8bc0fd71a8f829e424a57fe1de
Marcel Gregor <marcel_gregor@icloud.com>
2022-10-22 19:12:33 +0200

    prio Buttons styled

---
+++ script/addTask.js
@@ -0,0 +1,26 @@
+let buttons = ['urgent', 'medium', 'low']
+
+function prioButton(id) {
+    resetButtons();
+    let button = document.getElementById(id);
+    button.classList.add('active');
+    switch (id) {
+        case 'urgent':
+            button.classList.add('red')
+            break;
+        case 'medium':
+            button.classList.add('orange')
+            break;
+        case 'low':
+            button.classList.add('green')
+            break;
+        default:
+            break;
+    }
+}
+
+function resetButtons() {
+    buttons.forEach(button => {
+        document.getElementById(button).classList = 'prioButton';
+    });
+}
