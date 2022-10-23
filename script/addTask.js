15f477918690dbe3e66b03f65e7b1122dcd5a883
Marcel Gregor <marcel_gregor@icloud.com>
2022-10-23 16:00:44 +0200

    category selector

--- script/addTask.js
+++ script/addTask.js
@@ -1,7 +1,30 @@
-let buttons = ['urgent', 'medium', 'low']
+const buttons = ['urgent', 'medium', 'low'];
+
+let categorys = [
+    {
+        name: 'Sales',
+        color: 'pink'
+    },
+    {
+        name: 'Backoffice',
+        color: 'yellow'
+    },
+];
+
+const colors = [
+    'orange',
+    'red',
+    'green',
+    'blue',
+    'pink',
+    'yellow',
+    'ocean'
+];
+
+let categoryColor = '';
 
 function prioButton(id) {
-    resetButtons();
+    resetPrioButtons();
     let button = document.getElementById(id);
     button.classList.add('active');
     switch (id) {
@@ -19,7 +42,7 @@ function prioButton(id) {
     }
 }
 
-function resetButtons() {
+function resetPrioButtons() {
     buttons.forEach(button => {
         document.getElementById(button).classList = 'prioButton';
     });
@@ -29,26 +52,95 @@ function toggleMenu(id) {
     document.getElementById(id).classList.toggle('open');
 }
 
-function newCategory() {
+function renderNewCategory() {
     document.getElementById('colorPicker').style.display = 'flex';
     toggleMenu('category');
     document.getElementById('category').innerHTML = newCategoryTemplate();
 }
 
+function addCategory() {
+    let input = document.getElementById('categoryInput');
+    if (input.value) {
+        categorys.push({
+            name: input.value,
+            color: categoryColor
+        })
+    };
+    categoryColor = '';
+    renderCategorys();
+    document.getElementById('colorPicker').style.display = 'none';
+    let index = (categorys.length - 1).toString();
+    showCategory(index);
+    toggleMenu('category');
+}
+
+function colorPicker(id) {
+    resetPicker();
+    let choosedColor = document.getElementById(id);
+    choosedColor.classList.add('active');
+    categoryColor = id;
+}
+
+function resetPicker() {
+    colors.forEach(color => {
+        document.getElementById(color).classList = 'color';
+    });
+}
+
+function renderCategorys() {
+    document.getElementById('category').innerHTML = categorysTemplate();
+    renderSingleCategorys();
+    document.getElementById('colorPicker').style.display = 'none';
+}
+
+function showCategory(id) {
+    let firstValue = document.getElementById('firstValue');
+    firstValue.innerHTML = renderChoosenCategory(id);
+    toggleMenu('category');
+
+}
+
 
 /***********************HTML Templates**************************/
-
 
 function newCategoryTemplate() {
     return /*html*/`
     <div class="newCategory">
-        <input class="noBorder" placeholder="New category name" type="text">
+        <input id="categoryInput" class="noBorder" placeholder="New category name" type="text">
         <div class="createClearContainer">
-            <img src="./assets/clear.svg" alt=""> |
-            <img src="./assets/createTask.svg" alt="">
+            <img onclick="renderCategorys()" src="./assets/clear.svg" alt=""> |
+            <img onclick="addCategory()" src="./assets/createTask.svg" alt="">
         </div>
-        
     </div>
-    `
+    `;
+};
 
+
+function renderSingleCategorys() {
+    for (let i = 0; i < categorys.length; i++) {
+        const category = categorys[i];
+        document.getElementById('category').innerHTML += `
+       <span onclick="showCategory('category-${i}')" id="category-${i}" class="selectable">${category.name}
+           <div class="color ${category.color}"></div>
+       </span> 
+       `
+    }
+}
+
+function categorysTemplate() {
+    return /*html*/`
+        <span id="categoryPlaceholder" class="placeholder" onclick="toggleMenu('category')">
+            <div id="firstValue">Select task Category</div>
+            <img src="./assets/selectArrow.svg" alt="">
+        </span>
+        <span onclick="renderNewCategory()" class="selectable">New Category</span>
+`}
+
+function renderChoosenCategory(id) {
+    index = id.slice(-1);
+    let category = categorys[index]
+    return /*html*/ `
+            ${category.name}
+            <div class="color ${category.color}"></div>
+    `
 }
