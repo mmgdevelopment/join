0cbc5000372175a8e58d6c9fce665cba592cfd15
Steven Munk <s.f.munk@gmx.de>
2022-10-24 18:13:58 +0200

    save during work

--- login.js
+++ login.js
@@ -1,7 +1,23 @@
 /**
+ * function leads to login page with delay
+ */
+function goToLoginPageDelay() {
+    setTimeout(goToLoginPage, 500);
+}
+
+
+/**
+ * function leads to login page
+ */
+function goToLoginPage() {
+    window.location.href = 'login.html';
+}
+
+
+/**
  * function loads all saved users
  */
- function loadUsers() {
+function loadUsers() {
     let usersAsString = localStorage.getItem('allUsers');
     users = JSON.parse(usersAsString);
     console.log('Loaded Users ', users);
@@ -11,12 +27,12 @@
 /**
  * function creates a new user and pushes him into users(Array).
  */
- function signUp() {
+function signUp() {
     let username = document.getElementById('username');
     let email = document.getElementById('email');
     let password = document.getElementById('password');
 
-    users.push({username: username.value, email: email.value, password: password.value});
+    users.push({ username: username.value, email: email.value, password: password.value });
 
     saveNewUser();
 
@@ -42,26 +58,58 @@ function login() {
 
     let user = users.find(u => u.email == email.value && u.password == password.value);
     console.log(user);
-    if(user) {
+    if (user) {
         window.location.href = 'summary.html?msg=Du hast dich erfolgreich eingeloggt!';
     } else {
+        turnInputRed();
         password.value = '';
-        alert('Dein Passwort oder deine E-Mail ist nicht korrekt!');
     }
 }
 
 
 /**
- * function should send an email to existing user to resert password
+ * function turns inputborder red for a short duration
  */
-function sendPasswordResetMail() {
+function turnInputRed() {
+    document.getElementById('email-container').style = 'border: 1px solid #ff0000;';
+    document.getElementById('password-container').style = 'border: 1px solid #ff0000;';
+    setTimeout(turnInputGray, 2500);
+}
+
+
+/**
+ * function turns inputborder gray
+ */
+function turnInputGray() {
+    document.getElementById('email-container').style = 'border: 1px solid #D1D1D1;';
+    document.getElementById('password-container').style = 'border: 1px solid #D1D1D1;';
+}
+
+
+
+let noPassword = [];
+
+/**
+ * function checks the email and leads the user to reset.html
+ */
+function goToResetPage() {
     let email = document.getElementById('email');
 
     let user = users.find(u => u.email == email.value);
-    if(user) {
-        //send an email with reset password link,,,, dont know how this works yet
+    if (user) {
+        noPassword.push(user);
+        window.location.href = 'reset.html?msg=Bitte gib ein neues Passwort ein!';
+        createANewPassword();
     } else {
         email.value = '';
         alert('Es existier kein Account mit dieser E-Mail!');
     }
+}
+
+
+
+
+function createANewPassword() {
+    let user = noPassword[0];
+    console.log(user);
 }
