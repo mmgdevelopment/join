02275f5deb3a84294dd591b0c06557f7d9aac348
Icetune <109099573+Icetune@users.noreply.github.com>
2022-11-06 16:52:55 +0100

    added summary js

--- script/summary.js
+++ script/summary.js
@@ -5,6 +5,24 @@ let users = [];
 let email;
 let username;
 
+let epicsArray = [
+    {
+        "name": "Backoffice",
+        "color": "blue",
+        "tasks": []
+    },
+    {
+        "name": 'Marketing',
+        "color": 'red',
+        "tasks": []
+    },
+    {
+        "name": 'Development',
+        "color": 'orange',
+        "tasks": []
+    }
+]
+
 
 /**
  * function renders summary page and loads array with users form backend
@@ -15,9 +33,7 @@ async function init() {
     await downloadFromServer();
     users = JSON.parse(backend.getItem('users')) || [];
     checkForGuestUser();
-
-
-    console.log(users);
+    renderPage();
 }
 
 
@@ -124,7 +140,7 @@ function checkForGuestUser() {
  * function adds guest account on first place in users JSON
  */
 function addGuestToUsers() {
-    users.unshift({ username: 'Guest', email: 'guest@mail.com', password: 'guest1234', epics: Array(7) });
+    users.unshift({ username: 'Guest', email: 'guest@mail.com', password: 'guest1234', epics: epicsArray });
 }
 
 
@@ -143,39 +159,124 @@ function goToBorad() {
 /////////////////////////////////////////////////////////////////////////////////////////////
 
 
-let taskInBoard;
-let taskInProgress;
+let workingStages = [];
+let urgency = [];
+let allDueDates = [];
+
+let inBoard;
+let inProgress;
 let awaitingFeedback;
 
-let urgency = [];
+let year;
+let day;
+let month;
 
 let toDo;
 let done;
 
 
-
-
-
-
-
-function renderMidCard() {
-    let indexOfUser = users.findIndex(u => u.email == email);
-    let epics = users[indexOfUser]['epics'];
-
-    for (let i = 0; i < epics.length; i++) {
-        const epic = epics[i];
-        // getAllTasks(epic);
-        for (let i = 0; i < epic["tasks"].length; i++) {
-            const task = epic["tasks"][i];
-            urgency.push({ 'prio': task['prio'], 'date': task['dueDate'] });
-        }
-    }
-    showHighestPrio();
+/**
+ * function renders the whole summary page
+ * init
+ */
+function x() {
+    renderPage();
 }
 
 
+/**
+ * function renders whole page
+ */
+function renderPage() {
+    getAllInfoFromUsers();
+    renderAllWorkingStages();
+    renderHighestPrio();
+}
 
-function showHighestPrio() {
+
+/**
+ * This function goes through all epics of the database.
+ */
+
+function getAllInfoFromUsers() {
+    let indexOfUser = users.findIndex(u => u.email == email);
+    let epics = users[indexOfUser]['epics'];
+    for (let i = 0; i < epics.length; i++) {
+        const epic = epics[i];
+        getAllTasks(epic);
+    }
+}
+
+/**
+   * This function goes through all tasks of each epic
+   * @param {object} epic
+   */
+
+function getAllTasks(epic) {
+    console.log(epic["tasks"].length);
+    // for (let i = 0; i < epic["tasks"].length; i++) {
+    //     const task = epic["tasks"][i];
+    //     pushPrioAndDate(task);
+    //     pushAllWorkingStages(task);
+    // }
+}
+
+
+/**
+ * function pushes the prio of all tasks and their due date into urgency json
+ * @param {array} task 
+ */
+ function pushPrioAndDate(task) {
+    urgency.push({ 'prio': task['prio'], 'date': task['dueDate'] });
+}
+
+
+/**
+ * function pushes all categories into array
+ * @param {array} task 
+ */
+function pushAllWorkingStages(task) {
+    workingStages.push(task['category']);
+}
+
+
+/**
+ * function rendersall amounts of tasks in diffrent working stages
+ */
+function renderAllWorkingStages() {
+    getAllTasksNumbers();
+    displayAllTaksNumbers();
+}
+
+
+/**
+ * function gets all amounts of tasks in diffrent working stages and puts them into variables
+ */
+function getAllTasksNumbers() {
+    toDo = (workingStages.filter(x => x === 'todo').length);
+    inProgress = (workingStages.filter(x => x === 'progress').length);
+    awaitingFeedback = (workingStages.filter(x => x === 'feedback').length);
+    done = (workingStages.filter(x => x === 'done').length);
+    inBoard = (toDo + inProgress + awaitingFeedback);
+}
+
+
+/**
+ * function displays all amounts of tasks in diffrent working stages
+ */
+function displayAllTaksNumbers() {
+    document.getElementById('inBoard').innerHTML = inBoard;
+    document.getElementById('inProgress').innerHTML = inProgress;
+    document.getElementById('awaitingFeedback').innerHTML = awaitingFeedback;
+    document.getElementById('toDo').innerHTML = toDo;
+    document.getElementById('done').innerHTML = done;
+}
+
+
+/**
+ * function renders the task with the highest priority
+ */
+function renderHighestPrio() {
     if (urgencyContains('urgent') > 0) {
         displayHighestPrioTask('urgent');
     } else {
@@ -191,13 +292,22 @@ function showHighestPrio() {
     }
 }
 
+
+/**
+ * function checks if and how often a certain urgency is given in epics
+ * @param {string} value 
+ * @returns number
+ */
 function urgencyContains(value) {
     return urgency.filter((v) => (v.prio === value)).length;
 }
 
 
+/**
+ * function displays the task with the highest priority in middle card
+ * @param {string} value 
+ */
 function displayHighestPrioTask(value) {
-
     let prio = value.charAt(0).toUpperCase() + value.slice(1);
     let prioAmount = urgencyContains(value);
 
@@ -206,118 +316,68 @@ function displayHighestPrioTask(value) {
     document.getElementById('prio-img').src = `./assets/${value}.svg`;
     document.getElementById('prio-img-container').classList.add(value);
 
+    displayDueDate(value);
+}
 
 
+/**
+ * function displays the due date with the highest priority in middle card
+ * @param {string} value 
+ */
+function displayDueDate(value) {
+    pushAllDueDatesInArray(value);
+    getNearestDueDate();
+    let nearestDeadline = `${month} ${day}, ${year}`
+    document.getElementById('deadline').innerHTML = nearestDeadline;
+}
 
-    let d = urgency.filter((v) => (v.prio === value));
-    let dates = [];
 
+/**
+ * function pushes all due dates with highest urgency into array
+ * @param {string} value 
+ * @param {array} allDueDates 
+ */
+function pushAllDueDatesInArray(value) {
+    let allHighestPrio = urgency.filter((v) => (v.prio === value));
     for (let i = 0; i < urgencyContains(value); i++) {
-        const element = d[i]['date'];
-        dates.push(element);
+        const element = allHighestPrio[i]['date'];
+        allDueDates.push(element);
     }
-    dates = dates.sort();
-
-    let dateInNumbers = dates[0];
-
-    let year = dateInNumbers.substr(0, 4);
+}
 
 
-    let day = dateInNumbers.substr(8, 2);
+/**
+ * function finds the nearest due date from all dates with highest urgency
+ */
+function getNearestDueDate() {
+    allDueDates = allDueDates.sort();
+    let dateInNumbers = allDueDates[0];
+    year = dateInNumbers.substr(0, 4);
+    day = dateInNumbers.substr(8, 2);
+    getMonthInLetters(dateInNumbers);
+}
 
 
+/**
+ * function changes month number into month written
+ * @param {number} dateInNumbers 
+ */
+function getMonthInLetters(dateInNumbers) {
     let monthNumber = + (dateInNumbers.substr(5, 2) - 1);
     let allMonths = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    let month;
-
     for (let i = 0; i < 12; i++) {
         if (monthNumber == i) {
             month = allMonths[i];
         }
-
     }
-
-
-    let nearestDeadline = `${month} ${day}, ${year}`
-
-    document.getElementById('deadline').innerHTML = nearestDeadline;
-
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////
 
 
 function displayNoTasks() {
