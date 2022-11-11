d551177803fc623d44c8980eb327021d3ef6dd77
Kevin Sven Lentz <kevin.s.lentz@web.de>
2022-11-11 11:28:48 +0100

    changed findtaskid to findtaskbyid

--- script/drag_and_drop.js
+++ script/drag_and_drop.js
@@ -139,7 +139,7 @@ function getTasksCategory(task, epic) {
  */
 function renderDummys() {
   kanbanCategorys.forEach((category) => {
-    if (findTaskId(currentDraggedTask)["category"] != category) {
+    if (findTaskById(currentDraggedTask)["category"] != category) {
       document.getElementById(category + "-tasks").innerHTML +=
         dummyCardHTML(category);
     }
@@ -206,7 +206,7 @@ function allowDrop(ev) {
  * @param {string} category
  */
 function moveTo(category) {
-  let draggedTask = findTaskId(currentDraggedTask);
+  let draggedTask = findTaskById(currentDraggedTask);
   draggedTask["category"] = category;
   getAllEpics(user);
   saveData();
@@ -218,7 +218,7 @@ function moveTo(category) {
  * @returns task array
  */
 
-function findTaskId(id) {
+function findTaskById(id) {
   for (let j = 0; j < user["epics"].length; j++) {
     const epic = user["epics"][j];
     for (let i = 0; i < epic["tasks"].length; i++) {
@@ -236,7 +236,7 @@ function findTaskId(id) {
  * @returns epic
  */
 
-function findTaskEpic(id) {
+function findEpicById(id) {
   for (let j = 0; j < user["epics"].length; j++) {
     const epic = user["epics"][j];
     for (let i = 0; i < epic["tasks"].length; i++) {
@@ -378,7 +378,7 @@ function tickCheckBox(task) {
  */
 
 function taskIsDone(id) {
-  let task = findTaskId(id.toString().slice(1));
+  let task = findTaskById(id.toString().slice(1));
   let subtaskNumber = id.toString().slice(0, 1);
   if (task["subtasks"][subtaskNumber].checked) {
     task["subtasks"][subtaskNumber].checked = false;
@@ -419,7 +419,6 @@ function checkSubtasksDone(task) {
 /**
  * This function calc the progress in percent
  *
- * 
  * @param {object} task
  * @returns {number} how long the progressbar is
  */
@@ -446,8 +445,8 @@ async function saveData() {
  */
 function openCard(id) {
   cardWasOpened = true;
-  let task = findTaskId(id);
-  let epic = findTaskEpic(id);
+  let task = findTaskById(id);
+  let epic = findEpicById(id);
   document.getElementById('opened-card-container').setAttribute('onclick',`closeCard('${id}')`);
   document.getElementById("opened-card-container").classList.remove("d-none");
   document.getElementById("opened-card-container").innerHTML = renderTaskCard(
@@ -464,7 +463,7 @@ function openCard(id) {
  */
 function closeCard(id) {
   document.getElementById("opened-card-container").classList.add("d-none");
-  checkSubtaskAmount(findTaskId(id))
+  checkSubtaskAmount(findTaskById(id))
 }
 
 /**
@@ -473,4 +472,18 @@ function closeCard(id) {
  */
 function dontClose(event) {
   event.stopPropagation();
+}
+
+function filterTask(){
+        let search = document.getElementById("search").value;
+        search = search.toLowerCase();
+        for (let i = 0; i < allPokemon["count"]; i++) {
+          const pokemon = allPokemon["results"][i]["name"];
+      
+          if (printSearch < 10) {
+            searchData(pokemon, search);
+          }
+        }
+     
+      
 }
--- script/html.js
+++ script/html.js
@@ -5,25 +5,21 @@
  * @param {object} epic
  */
 
- function renderTask(task, epic) {
-    return `
+function renderTask(task, epic) {
+  return /*html*/ `
       <div onclick="openCard('${task["id"]}')" draggable="true" ondragstart="startDragging('${task["id"]}')" class="task-card">
       <span class="epic ${epic["color"]}">${epic["name"]}</span>
       <h4 class="task-name">${task["title"]}</h4>
       <p class="task-description">${task["description"]}</p>
-  
-
       <div id="subtasks${task["id"]}" class="d-flex-jc-sb w-100">
-      
       </div>
-  
       <div class="d-flex-jc-sb w-100">
-          <div id="assigned${task['id']}" class="contacts">
+          <div id="assigned${task["id"]}" class="contacts">
           </div><img src="assets/${task["prio"]}.svg">
       </div>
   </div>`;
-  }
- 
+}
+
 /**
  * This function is used to render the tasks on the opened card board.html
  *
@@ -31,94 +27,99 @@
  * @param {object} epic
  */
 
-  function renderTaskCard(task, epic) {
-    return `
+function renderTaskCard(task, epic) {
+  return /*html*/ `
       <div onclick="dontClose(event)" class="task-card">
       <span class="epic ${epic["color"]}">${epic["name"]}</span>
       <h4 class="task-name">${task["title"]}</h4>
       <p class="task-description">${task["description"]}</p>
       <div class="wrapper">
       <div>
-    
       <div><span>Due date:</span> ${task["dueDate"]}</div>
       <div><span>Priotity:</span><span class="priority ${task["prio"]}">${task["prio"]}<img class="prio_img" src="assets/${task["prio"]}.svg"</img></span></div>
-      <div  id="contacts-on-card"><span>Assigned To:</span><div id="assignedCard${task['id']}">
+      <div  id="contacts-on-card"><span>Assigned To:</span><div id="assignedCard${task["id"]}">
       </div> </div>
-         
-
-      
   </div> 
-  
   <div id="openCardSubtasks">
-
   </div>
    <img onclick="closeCard('${task["id"]}')" class="close-btn" src="assets/close.svg"</img>
-  <img onclick="closeCard()" class="edit-btn" src="assets/pencil_filled_square.svg"</img>`
-  
-  }
-  /*<span class="underlined" >Subtasks</span>*/
+  <img onclick="closeCard()" class="edit-btn" src="assets/pencil_filled_square.svg"</img>`;
+}
+/*<span class="underlined" >Subtasks</span>*/
 
-function renderSubtaskBarHTML(id, task, done, barProgress){
-        document.getElementById('subtasks'+ id).innerHTML = `
-        <div class="bar" style="background-image:  linear-gradient(to right, #29ABE2 ${barProgress}%,#29ABE2 ${barProgress/100}px, #f4f4f4 0%)"></div><span>${done}/${task['subtasks'].length} Done</span>`
+
+
+/**
+ * This function renders the progressbar of the subtasks
+ * 
+ * @param {string} id 
+ * @param {object} task 
+ * @param {number} done 
+ * @param {number} barProgress 
+ */
+function renderSubtaskBarHTML(id, task, done, barProgress) {
+  document.getElementById("subtasks" + id).innerHTML = `
+        <div class="bar" style="background-image:  linear-gradient(to right, #29ABE2 ${barProgress}%,#29ABE2 ${
+    barProgress / 100
+  }px, #f4f4f4 0%)"></div><span>${done}/${task["subtasks"].length} Done</span>`;
 }
 
 function renderSubtaskHTML(name, i, id) {
-    openCardSubtasks
-    return /* html*/`
-    <div class="d-flex-gap-20"><input id="subtaskCheckbox${i}" class="checkbox" type="checkbox" onclick="taskIsDone('${i + id}')"><div class="subtask-titel">${name}</div>`
+  openCardSubtasks;
+  return /* html*/ `
+    <div class="d-flex-gap-20"><input id="subtaskCheckbox${i}" class="checkbox" type="checkbox" onclick="taskIsDone('${
+    i + id
+  }')"><div class="subtask-titel">${name}</div>`;
 }
 
-  /**
+/**
  * This function is used to render the assigned people on board.html
  * The function will happen after renderTask, because its rendered in that tenplate.
  *
  * @param {object} task
  * @param {string} contact
  */
-  function renderAssignedContactsHTML(contact, task){     
-    document.getElementById('assigned'+ task['id']).innerHTML += `
-    <div class="contact">${contact}</div>`
-    
+function renderAssignedContactsHTML(contact, task) {
+  document.getElementById("assigned" + task["id"]).innerHTML += `
+    <div class="contact">${contact}</div>`;
 }
 
-function renderCardContactsHTML(contact, task, contactName){      
-    document.getElementById('assignedCard'+ task['id']).innerHTML += `
-   <div class="d-flex-gap-20"><div class="contact-on-card">${contact}</div><span class="contact-name">${contactName}</span></div>  `
-    
+function renderCardContactsHTML(contact, task, contactName) {
+  document.getElementById("assignedCard" + task["id"]).innerHTML += `
+   <div class="d-flex-gap-20"><div class="contact-on-card">${contact}</div><span class="contact-name">${contactName}</span></div>  `;
 }
 
 /**
  * This function removes the searchbar and puts it in Desktop view and changes the wording of the Addtask-Button
- * On board.html 
+ * On board.html
  *
  */
- function renderDesktopView() {
-    document.getElementById("btn-add-task").innerHTML = "";
-    document.getElementById("btn-add-task").innerHTML = 'Add Task <img src="assets/plus-white.svg">';
-    document.getElementById("mobile-search").innerHTML = "";
-    document.getElementById("desktop-search").innerHTML =
-      '<input id="search" placeholder="Find task" type="text"></input>';
-  }
+function renderDesktopView() {
+  document.getElementById("btn-add-task").innerHTML = "";
+  document.getElementById("btn-add-task").innerHTML =
+    'Add Task <img src="assets/plus-white.svg">';
+  document.getElementById("mobile-search").innerHTML = "";
+  document.getElementById("desktop-search").innerHTML =
+    '<input id="search" placeholder="Find task" type="text"></input>';
+}
 
-  /**
+/**
  * This function removes the searchbar and puts it in Mobile view and changes the wording of the Addtask-Button
  * On board.html
  *
  */
 function renderMobileView() {
-    document.getElementById("btn-add-task").innerHTML = "";
-    document.getElementById("btn-add-task").innerHTML =
-      '<img src="assets/plus-white.svg">';
-    document.getElementById("desktop-search").innerHTML = "";
-    document.getElementById("mobile-search").innerHTML =
-      '<input id="search" placeholder="Find task" type="text"></input>';
-  }
-  
-  
-  function dummyCardHTML(id){
-    return `<div class="dummy-card" id="dummy-card${id}-tasks">
+  document.getElementById("btn-add-task").innerHTML = "";
+  document.getElementById("btn-add-task").innerHTML =
+    '<img src="assets/plus-white.svg">';
+  document.getElementById("desktop-search").innerHTML = "";
+  document.getElementById("mobile-search").innerHTML =
+    '<input id="search" placeholder="Find task" type="text"></input>';
+}
+
+function dummyCardHTML(id) {
+  return `<div class="dummy-card" id="dummy-card${id}-tasks">
     <div>
     </div>
-    </div>`
-  }
+    </div>`;
+}
