e52141e85437080bb6f4b1f0e374fe88d1e3b18c
Marcel Gregor <marcel_gregor@icloud.com>
2022-11-21 17:35:47 +0100

    bugfix

--- script/drag_and_drop.js
+++ script/drag_and_drop.js
@@ -529,7 +529,7 @@ function findCategoryById(id) {
   let category;
   for (let i = 0; i < user.epics.length; i++) {
     const epic = user.epics[i];
-    for (let j = 0; i < epic.tasks.length; j++) {
+    for (let j = 0; j < epic.tasks.length; j++) {
       if (!category) {
         category = user.epics.find(epic =>
           epic.tasks[j].id == id
